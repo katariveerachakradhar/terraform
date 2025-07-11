37f806b12b0d8358340b27dbed0658a5bd6bf088
Veera <veeravamshi1@gmail.com>
2025-07-11 10:43:03 +0530

    multiple-accounts

---
+++ multiple-accounts/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+}
+
+provider "aws" {
+  #Configuration options
+}
+
+# provider "aws" {
+#   # Configuration options
+#   alias = "dev"
+#   profile = "dev"
+# }
